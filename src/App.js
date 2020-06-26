c7432904f570af33215243ecff9cafaf0725edda
PetrovIlyuha <dnamix1@gmail.com>
2020-06-26 17:06:37 +0300

    laid down routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Home from "./components";
+import Cv from "./components/Cv";
+
+import { Route } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/cv" component={Cv} />
     </div>
   );
 }
