e9527f6374729ddb71887bc29d1acd8efbc0565a
PetrovIlyuha <dnamix1@gmail.com>
2020-06-27 10:02:17 +0300

    added routes that were left untouched

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./App.css";
 import Home from "./components";
 import Cv from "./components/Cv";
+import Contacts from "./components/Contacts";
 
 import { Route } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <CssBaseline />
       <Route exact path="/" component={Home} />
       <Route path="/cv" component={Cv} />
+      <Route path="/contacts" component={Contacts} />
+      <Route path="/portfolio" component={Projects} />
     </div>
   );
 }
