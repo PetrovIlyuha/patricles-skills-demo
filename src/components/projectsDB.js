096700ef1aa5b1757544fa853afe4dbe77a9a4c9
PetrovIlyuha <dnamix1@gmail.com>
2020-06-29 14:10:17 +0300

    Data layer

---
+++ src/components/projectsDB.js
@@ -0,0 +1,27 @@
+import TeahouseDemo from "../images/teahouse_demo.gif";
+import Insta from "../images/insta_demo.gif";
+import NightWatcher from "../images/movies.gif";
+
+export const projectsList = [
+  {
+    name: "Tea House",
+    description:
+      "E-commerce Single-Page Application powered by React, FormSpree and Contentful CMS",
+    image: TeahouseDemo,
+    link: "https://zenteahouse.netlify.app/",
+  },
+  {
+    name: "Instagram Clone",
+    description:
+      "Instagram Clone with React, Material UI, Cloudinary - Static Files CDN, GraphQL Apollo on the FrontEnd, Hasura Backend, GraphQL schema language for serving requests to Hasura DB",
+    image: Insta,
+    link: "https://instaclone-4557lfzzp.now.sh/",
+  },
+  {
+    name: "NightWatcher (Movie Selection)",
+    description:
+      "Movie Selection and Quick Assessment Guide powered by React, Styled-Components and MovieDB",
+    image: NightWatcher,
+    link: "https://nightwatcher.netlify.app/",
+  },
+];
